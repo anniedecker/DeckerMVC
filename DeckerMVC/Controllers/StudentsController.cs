952c0304d0673df1e4f801c0573d3617ce246026
Annie Decker <anniedecker@ou.edu>
2019-11-06 11:31:18 -0600

    initial commit

---
+++ DeckerMVC/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using DeckerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeckerMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            Student s1 = new Student()
+            {
+                StudentID = 111,
+                FirstName = "Annie",
+                LastName = "Decker",
+                FavoriteColor = "Gray"
+            };
+
+            Student s2 = new Student()
+            {
+                StudentID = 2,
+                FirstName = "Katherine",
+                LastName = "Harris",
+                FavoriteColor = "Blue"
+            };
+
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            return View(students);
+        }
+
+       
+        
+        
+    }
+}
---
+++ DeckerMVC/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeckerMVC.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+        
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+    }
+
+}
